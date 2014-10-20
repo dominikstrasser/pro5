c432325867e6f49bc495e65f7bc661e07d259f97
Dominik Strasser <strasserdominik@hotmail.com>
2014-10-20 15:45:39 +0200

    Kommentare Entfernt

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var db = require("./database/dbConnection.js");
 var routes = require("./routes/index.js");
 
-//ASDFASDF
-// yihhaaaaaa
 
 db.once("open", function(){
     console.log("db open");
