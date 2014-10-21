ca85f5e00469b7d18c71b68f0d46380b8d4c37df
Dominik Strasser <strasserdominik@hotmail.com>
2014-10-21 21:48:48 +0200

    lokale mongodb kann zurückgesetzt werden
    einer der beiden mongoose.connect Funktionen muss auskommentiert werden…

--- database/resetDatabase/reset.js
+++ database/resetDatabase/reset.js
@@ -4,7 +4,9 @@ var moment = require('moment');
 
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://testUser:mtd_pro5@ds063859.mongolab.com:63859/pro5_hotel");
+//mongoose.connect("mongodb://testUser:mtd_pro5@ds063859.mongolab.com:63859/pro5_hotel");
+mongoose.connect('mongodb://localhost/pro5_hotel');
+
 var db = mongoose.connection;
 
 var roomModel = require("./../model/room.js");
