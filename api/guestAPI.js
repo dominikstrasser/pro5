58d55d1019f29c2b41378e5ca6dbde4b26738a9c
Dominik Strasser <strasserdominik@hotmail.com>
2014-10-23 22:20:58 +0200

    add guests

--- api/guestAPI.js
+++ api/guestAPI.js
@@ -1,8 +1,18 @@
 var router = require("express").Router();
 var guestModel = require("./../database/model/guest.js");
 router.get("/", function (req, res) {
-    console.log("guestAPI : /");
+    console.log("guestAPI - get : /");
     guestModel.find(function (err, result) {
+        if (err) throw new Error(err);
+        res.json(result);
+    });
+});
+
+router.post("/", function (req, res) {
+    console.log("guestAPI - post : /");
+    var guest = new guestModel(req.body);
+    console.log(guest);
+    guest.save(function (err, result) {
         if (err) throw new Error(err);
         res.json(result);
     });
