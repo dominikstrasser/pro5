ea239e1a89d5068edeb8d19024a8c713bb16c5ed
Dominik Strasser <strasserdominik@hotmail.com>
2014-10-28 09:33:57 +0100

    Änderung max

--- public/services.js
+++ public/services.js
@@ -1,7 +1,11 @@
 angular.module('pro5_hzv.bookingService', ['ngResource'])
-	.factory('bookingService', function($resource){
+    .factory('arrivalService', function($resource){
         return $resource('api/booking/getCurrentArrivals/', {}, {
             currentArrivals: {method:'GET', params:{}, isArray:true}
         });
     }
-	);
+).factory('departureService', function($resource){
+        return $resource('api/booking/getCurrentDepartures/', {},{
+            currentDepartures: {method:'GET', params:{}, isArray:true}
+        });
+    });
