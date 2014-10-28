37883e6c233b88f881f8d447e4a740b0dcc51f25
markoarnauto <markustretzmueller@gmail.com>
2014-10-28 16:22:08 +0100

    GuestFormular

--- public/app.js
+++ public/app.js
@@ -10,13 +10,18 @@ angular.module('pro5_hzv', [
     'pro5_hzv.guests',
     'pro5_hzv.management',
     'angular-momentjs',
-    'pro5_hzv.bookingService'
+    'pro5_hzv.bookingService',
+    'pro5_hzv.guestService'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/dashboard'});
+}])
+    .controller("indexCtrl", ['$scope','guestMainDataService', function ($scope, guestMainDataService) {
+    $scope.guest = {};
+    $scope.guest.id = '544f503e31925fc402810f03';
+    guestMainDataService.getSingleGuest(function(data) {
+        $scope.guest = data[0];
+    });
+    console.log('indexCtrl working!');
+    console.log($scope.guest);
 }]);
-
-// Hallo
-angular.module("pro5_hzv").controller("testController", function($scope){
-    $scope.currentUser = "currentUser - Objekt";
-});
