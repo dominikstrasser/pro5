0fba9abf83559fa94ebfc56240a4e9804ace1793
Dominik Strasser <strasserdominik@hotmail.com>
2014-10-28 09:31:30 +0100

    Änderung max

--- public/views/dashboard/dashboard.js
+++ public/views/dashboard/dashboard.js
@@ -4,16 +4,18 @@ angular.module('pro5_hzv.dashboard', [
     'ngRoute'
 ])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/dashboard', {
-    templateUrl: 'views/dashboard/dashboard.html',
-    controller: 'dashboardCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/dashboard', {
+            templateUrl: 'views/dashboard/dashboard.html',
+            controller: 'dashboardCtrl'
+        });
+    }])
 
-.controller('dashboardCtrl', ['$scope', '$http', '$moment', 'bookingService', function($scope, $http, $moment, bookingService) {
+    .controller('dashboardCtrl', ['$scope', '$moment', 'arrivalService', 'departureService', function($scope, $moment, arrivalService, departureService) {
+
 
         $scope.arrivals = {};
+        $scope.departures = {};
         var setTimeTo12 = function(d){
             d.hour(12);
             d.seconds(0);
@@ -21,8 +23,40 @@ angular.module('pro5_hzv.dashboard', [
             d.milliseconds(0);
         };
 
+        $scope.currentDate = "first";
+
+
+        $scope.isCurrentDate = function(date) {
+            var check = true;
+            var dateBuffer = date;
+            date = $moment(date);
+            if ($scope.currentDate != "first") {
+                if (date.isSame($scope.currentDate, "days")) {
+                    check = true;
+                } else {
+                    check = false;
+                }
+            }
+            $scope.currentDate = dateBuffer;
+            return check;
+        };
+
+        $scope.displayDate = function (d) {
+            var dateBuffer = d;
+            d = $moment(d);
+            if(d.isSame($scope.today, "days")){
+                return "Heute";
+            }else{
+                if(d.isSame($scope.tomorrow, "days")) {
+                    return "Morgen";
+                } else {
+                    return dateBuffer;
+                }
+            }
+        };
+
         $scope.checkDate = function(d){
-            d = moment(d);
+            d = $moment(d);
             if(d.isSame($scope.today, "days")) return 1;
             if(d.isSame($scope.tomorrow, "days")) return 2;
             if(d.isAfter($scope.tomorrow, "days")) return 3;
@@ -33,8 +67,7 @@ angular.module('pro5_hzv.dashboard', [
         setTimeTo12($scope.today);
         setTimeTo12($scope.tomorrow);
 
-        $scope.arrivals = bookingService.currentArrivals(function(){
+        $scope.arrivals = arrivalService.currentArrivals();
+        $scope.departures = departureService.currentDepartures();
 
-        });
-
-}]);
+    }]);
