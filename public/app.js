c7dac647dd406bce048afcf025e55e156348c44c
Dominik Strasser <strasserdominik@hotmail.com>
2014-10-28 19:26:04 +0100

    Nochmals den Datepicker geändert
    Der vorige Datepicker war auch nicht das wahre…
    der müsste jetzt passen
    http://mgcrea.github.io/angular-strap/##datepickers

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('pro5_hzv', [
     'ngRoute',
-    'ui.bootstrap',
+    'mgcrea.ngStrap.datepicker',
     'pro5_hzv.dashboard',
     'pro5_hzv.roomList',
     'pro5_hzv.requests',
@@ -17,6 +17,14 @@ angular.module('pro5_hzv', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/dashboard'});
 }])
+    .config(function($datepickerProvider) {
+        angular.extend($datepickerProvider.defaults, {
+            dateFormat: 'dd. MMMM yyyy',
+            startWeek: 1,
+            autoclose: "true"
+            //delay: { show: 500, hide: 100000 }
+        });
+    })
     .controller("indexCtrl", ['$scope','guestMainDataService', function ($scope, guestMainDataService) {
     $scope.guest = {};
     $scope.guest.id = '544f503e31925fc402810f03';
--- public/views/dashboard/dashboard.js
+++ public/views/dashboard/dashboard.js
@@ -13,7 +13,6 @@ angular.module('pro5_hzv.dashboard', [
 
     .controller('dashboardCtrl', ['$scope', '$moment', 'arrivalService', 'departureService', function($scope, $moment, arrivalService, departureService) {
 
-
         $scope.arrivals = {};
         $scope.departures = {};
         var setTimeTo12 = function(d){
@@ -70,7 +69,14 @@ angular.module('pro5_hzv.dashboard', [
         $scope.arrivals = arrivalService.currentArrivals();
         $scope.departures = departureService.currentDepartures();
 
-    }]).controller("requestFormController", function($scope, $http, $moment){
+    }])
+
+
+
+
+
+
+    .controller("requestFormController", function($scope, $http, $moment){
 
 
         var setTimeTo12 = function(d){
@@ -80,14 +86,6 @@ angular.module('pro5_hzv.dashboard', [
             d.milliseconds(0);
         };
 
-        $scope.requestForm = {};
-        $scope.requestForm.room_count = "2";
-        $scope.dateOptions = {
-            changeYear: true,
-            changeMonth: true,
-            yearRange: '2014:2020',
-            dateFormat: "dd mm yyyy"
-        };
 
         $http.get("api/room/")
             .success(function(data, status, headers, config){
@@ -96,63 +94,30 @@ angular.module('pro5_hzv.dashboard', [
             });
 
         $scope.$watchCollection("requestForm", function(newValue, oldValue){
-            if($moment(newValue.arr).isValid() && $moment(newValue.dep).isValid()) {
-                var data = {};
-                data.arr = $moment(newValue.arr);
-                setTimeTo12(data.arr);
-                data.dep = $moment(newValue.dep);
-                setTimeTo12(data.dep);
-                $http.post("api/booking/check", data)
-                    .success(function (data, status, headers, config) {
-                        $scope.remove_rooms = data;
-                        $scope.available_rooms = $scope.all_rooms.slice(0);
-                        for (var i = 0; i < $scope.available_rooms.length; i++) {
-                            for (var j = 0; j < data.length; j++) {
-                                if ($scope.available_rooms[i]._id == data[j].room_id[0]) {
-                                    $scope.available_rooms.splice(i,1);
-                                    i--;
-                                    break;
+            if(typeof newValue != 'undefined') {
+                if ($moment(newValue.arr).isValid() && $moment(newValue.dep).isValid()) {
+                    var data = {};
+                    data.arr = $moment(newValue.arr);
+                    setTimeTo12(data.arr);
+                    data.dep = $moment(newValue.dep);
+                    setTimeTo12(data.dep);
+                    $http.post("api/booking/check", data)
+                        .success(function (data, status, headers, config) {
+                            $scope.remove_rooms = data;
+                            $scope.available_rooms = $scope.all_rooms.slice(0);
+                            for (var i = 0; i < $scope.available_rooms.length; i++) {
+                                for (var j = 0; j < data.length; j++) {
+                                    if ($scope.available_rooms[i]._id == data[j].room_id[0]) {
+                                        $scope.available_rooms.splice(i, 1);
+                                        i--;
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                    });
+                        });
+                }
             }
         });
-
-
-        $scope.today = function() {
-            $scope.dt = new Date();
-        };
-        $scope.today();
-
-        $scope.clear = function () {
-            $scope.dt = null;
-        };
-
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-        };
-
-        $scope.toggleMin = function() {
-            $scope.minDate = $scope.minDate ? null : new Date();
-        };
-        $scope.toggleMin();
-
-        $scope.open = function($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-
-            $scope.opened = true;
-        };
-
-        $scope.dateOptions = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
 
 
     });
