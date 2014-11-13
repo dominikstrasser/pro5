e23728ca078352282253d7c932b8048390882662
Dominik Strasser <strasserdominik@hotmail.com>
2014-11-13 11:18:13 +0100

    E-Mail Korrespondenz auf request

--- public/services.js
+++ public/services.js
@@ -4,7 +4,8 @@ angular.module('pro5_hzv.dataService', ['ngResource'])
             currentArrivals: {method:'GET', url:'api/bookings/getCurrentArrivals/', isArray:true},
             currentDepartures: {method:'GET', url:'api/bookings/getCurrentDepartures/', isArray:true},
             check: {method: "GET", url:'api/bookings/check/', isArray:true},
-            update: {method: 'PUT' }
+            update: {method: 'PUT' },
+            detail: {method: "GET", url:'api/bookings/detail/', isArray:true}
         });
     }).factory('guestProvider', function($resource){
         return $resource('api/guests/:_id', {_id : "@_id"},{
@@ -12,6 +13,10 @@ angular.module('pro5_hzv.dataService', ['ngResource'])
         });
     }).factory('roomProvider', function($resource){
         return $resource('api/rooms/:_id', {},{
+            update: {method: 'PUT' }
+        });
+    }).factory('emailProvider', function($resource){
+        return $resource('api/emails/:_id', {},{
             update: {method: 'PUT' }
         });
     });
