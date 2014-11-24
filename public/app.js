6cf57ea122ea2267558adbd58bca3397d9238beb
Thomas Steidl <thomas.steidl@live.at>
2014-11-24 15:32:30 +0100

    dashboard css

--- public/app.js
+++ public/app.js
@@ -35,7 +35,7 @@ config(['$routeProvider', function($routeProvider) {
         $scope.active = false;
 
         $scope.getGuest = function (_id) {
-            guestProvider.get({ _id: '54648b111eef5c729338ae76' }, function(data) {
+            guestProvider.get({ _id: '5472f00e3fb30781458ad890' }, function(data) {
                 $scope.guest = data;
                 cGuestId = data._id;
             });
