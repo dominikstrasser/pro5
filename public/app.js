b300f6def0fce128b3ad6d2f81e0132773b0f286
Dominik Strasser <strasserdominik@hotmail.com>
2014-12-04 23:38:21 +0100

    test

--- public/app.js
+++ public/app.js
@@ -19,8 +19,8 @@ angular.module('pro5_hzv', [
     'pro5_hzv.dataService',
     'pro5_hzv.dragDropDirective',
     'pro5_hzv.roomListDirective',
-    'pro5_hzv.dashboardAktuellDirective',
-    'eee-c.angularBindPolymer'
+    'pro5_hzv.dashboardAktuellDirective'
+    //'eee-c.angularBindPolymer'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/dashboard'});
